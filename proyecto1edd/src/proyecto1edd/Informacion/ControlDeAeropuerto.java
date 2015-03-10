2799f887df7dcc92ac0c788a6487c3f615ac7654
201123065 <heyymarcos@gmail.com>
2015-03-10 06:56:31 -0600

    ProyectoVuelos
    ejemplo

---
+++ proyecto1edd/src/proyecto1edd/Informacion/ControlDeAeropuerto.java
@@ -0,0 +1,18 @@
+
+package proyecto1edd.Informacion;
+
+
+public class ControlDeAeropuerto {
+    private class Nodo{
+        public Nodo p;
+        public Nodo izq;
+        public Nodo der;
+        public int llave;
+        public Object valor;
+        
+    }
+    ControlDeAeropuerto(){
+        //control//
+    }
+    
+}
---
+++ proyecto1edd/src/proyecto1edd/Proyecto1edd.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1edd;
+
+/**
+ *
+ * @author VENTAS
+ */
+public class Proyecto1edd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Control op = new Control();
+       op.setVisible(true);
+    }
+    
+}
